333cad1de34b9041a2b57f689adea97462d0b373
Bob <bobevans547@gmail.com>
2017-09-07 10:58:59 -0400

    Pig Dice Complete

--- Pig Dice/Program.cs
+++ Pig Dice/Program.cs
@@ -1,15 +1,63 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pig_Dice
+namespace PigDiceProject
 {
     class Program
     {
+        Random rnd = new Random();
+
+
+        int GenerateDieNumber()
+        {
+            var DieNumber = rnd.Next(6) + 1;
+            Debug($"The die number is {DieNumber}");
+            return DieNumber;
+        }
+
+        void Debug(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        void RunGameOnce()
+        {
+            var total = 0;
+            var DieNumber = GenerateDieNumber();
+            while (DieNumber != 1)
+            {
+                total = total + DieNumber;
+                DieNumber = GenerateDieNumber();
+            }
+            Debug($"Total game score is {total}");
+
+        }
+        void Run()
+        {
+            bool PlayAgain = true;
+            while (PlayAgain == true)
+            {
+                RunGameOnce();
+                Console.Write($"Do you want to play again? Y/N : ");
+                var answer = Console.ReadLine();
+                if (answer == "Y" || answer == "y")
+                {
+                    PlayAgain = true;
+                }
+                else
+                {
+                    PlayAgain = false;
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
+            new Program().Run();
         }
-    }
-}
+    }}
